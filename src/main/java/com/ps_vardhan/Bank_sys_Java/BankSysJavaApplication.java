bb0f7be27fb754f63a25824d4bcb7d9bf4c56a1d
SV-000 <vardhan0584@gmail.com>
2025-08-13 14:48:20 +0530

    The Initial Build of the Code with all its Dependencies Installed from the Spring Initilizer

---
+++ src/main/java/com/ps_vardhan/Bank_sys_Java/BankSysJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ps_vardhan.Bank_sys_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSysJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSysJavaApplication.class, args);
+	}
+
+}
